490e77bbd131f7335b75370816df6107eb89ab20
jonathanbanerjee <jonathan@youd.net>
2024-08-16 16:49:49 +0100

    adding menu

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactPage: React.FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Contact Us Page</h1>
+    </div>
+  );
+};
+
+export default ContactPage;
