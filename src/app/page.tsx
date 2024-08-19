ea679ebd38ad36cf02bd08bb41bc4c4fe350dc49
jonathanbanerjee <jonathan@youd.net>
2024-08-19 14:08:52 +0100

    adding menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,104 @@
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src="/logo-main.jpg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      />
-    </main>
+    <header className="flex items-center justify-between bg-emerald-800 p-4">
+      <div className="flex items-center space-x-4">
+        <Image
+          src="/logo-main.jpg"
+          alt="Cafe Logo"
+          className="dark:invert rounded-md"
+          width={100}
+          height={100}
+        />
+        <h2 className={cn("text-white", "text-lg", "font-semibold")}>
+          Art Café
+        </h2>
+      </div>
+
+      <NavigationMenu>
+        <NavigationMenuList className="flex flex-wrap justify-center space-x-4 sm:space-x-6 md:space-x-8 lg:space-x-20">
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/"
+              className={cn(
+                "text-white",
+                "bg-transparent",
+                "rounded-lg",
+                "hover:bg-emerald-600",
+                "transition-colors",
+                "duration-200",
+                "p-2"
+              )}
+            >
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/menu"
+              className={cn(
+                "text-white",
+                "bg-transparent",
+                "rounded-lg",
+                "hover:bg-emerald-600",
+                "transition-colors",
+                "duration-200",
+                "p-2"
+              )}
+            >
+              Menu
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/about"
+              className={cn(
+                "text-white",
+                "bg-transparent",
+                "rounded-lg",
+                "hover:bg-emerald-600",
+                "transition-colors",
+                "duration-200",
+                "p-2"
+              )}
+            >
+              About
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              href="/contact"
+              className={cn(
+                "text-white",
+                "bg-transparent",
+                "rounded-lg",
+                "hover:bg-emerald-600",
+                "transition-colors",
+                "duration-200",
+                "p-2"
+              )}
+            >
+              Contact
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
   );
 }
