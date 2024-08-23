96e42b4638045185bd47a4fa2c18a97a10e99c95
jonathanbanerjee <jonathan@youd.net>
2024-08-23 15:11:09 +0100

    adjusting coordinatas

--- src/components/ui/map.tsx
+++ src/components/ui/map.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
-import { Card, CardContent, CardTitle } from "./card";
+import { Card, CardContent, CardDescription, CardTitle } from "./card";
 
 console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY + "hello world");
 const GoogleMap: FunctionComponent<Record<string, unknown>> = () => (
@@ -11,11 +11,15 @@ const GoogleMap: FunctionComponent<Record<string, unknown>> = () => (
         <CardContent>
           <Map
             style={{ width: "100%", height: "500px" }}
-            center={{ lat: 53.54992, lng: 10.00678 }}
+            center={{ lat: 51.546484, lng: -0.157013 }}
+            zoom={18}
           >
-            <Marker position={{ lat: 53.54992, lng: 10.00678 }} />
+            <Marker position={{ lat: 51.54653, lng: -0.15706 }} />
           </Map>
         </CardContent>
+        <CardDescription>
+          76 Haverstock Hill London NW3 2BE, United Kingdom
+        </CardDescription>
       </Card>
     </APIProvider>
   </>
